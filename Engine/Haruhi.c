e6f4c3ce8b41a626d178f544ddd1b2939247ebd5
Averi <averyb.personal@gmail.com>
2026-04-06 23:49:47 -0400

    Update Haruhi.c

--- Engine/Haruhi.c
+++ Engine/Haruhi.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 
 #include "Context/Boot/Entrypoint/HaruEntry.h"
+#include "Core/Engine/Engine.h"
+#include "Core/Application/Application.h"
+#include "Platform/Runtime/Window/Window.h"
 
 HaruEntry HaruMain() {
-    printf("Hello, from Haruhi");
+    HaruApplication Application = HaruApplicationInitialize();
 
-    return HARU_EXIT_FAILURE;
+    Application.Name = "Haruhi Engine";
+
+    if (HaruInitializeWindowing() != HARU_RESULT_SUCCESS) {
+        fprintf(stderr, "Haruhi windowing initialization failed.\n");
+    }
+
+    HaruWindow *WINDOW = HaruApplicationCreateWindow(&Application, "Haruhi Engine", 800, 600);
+    if (!WINDOW) {
+        fprintf(stderr, "Couldn't create window.");
+
+        return HARU_EXIT_FAILURE;
+    }
+
+    HaruApplicationAddWindow(&Application, WINDOW);
+
+    HaruApplicationRun(&Application);
+    HaruApplicationShutdown(&Application);
+
+    return HARU_EXIT_SUCCESS;
 }
