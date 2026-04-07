29f5a458c462efc1db62f82085c36d2a6cfa49d4
Averi <averyb.personal@gmail.com>
2026-04-06 23:49:38 -0400

    Create Window.h

---
+++ Engine/Platform/Runtime/Window/Window.h
@@ -0,0 +1,61 @@
+//
+//  Haruhi Engine
+//
+//  Original source code derived from Kunamo Engine:
+//    Copyright © 2026 Kunamo Entertainment. All rights reserved.
+//
+//  Modifications & Integration:
+//    Copyright © 2026 AveriC & Averi
+//
+
+#ifndef PWINDOW_H
+#define PWINDOW_H
+
+    #include "../../../Core/Types.h"
+
+    typedef enum {
+        HARU_WINDOW_BACKEND_GLFW,
+        HARU_WINDOW_BACKEND_SDL,
+        
+        HARU_WINDOW_BACKEND_NONE
+    } HaruWindowBackend;
+
+    typedef struct HaruApplication HaruApplication;
+
+    typedef struct {
+        HaruWindowBackend Backend;
+        
+        void *Handle;
+
+        HaruApplication *Application;
+
+        int X;
+        int Y;
+
+        int WIDTH;
+        int HEIGHT;
+
+        int FramebufferWidth;
+        int FramebufferHeight;
+
+        int ShouldClose;
+
+        int Focused;
+        int Minimized;
+        int Fullscreen;
+
+        float DPIScale;
+    } HaruWindow;
+
+    extern HaruWindowBackend gWindowBackend;
+
+    void HaruSetWindowBackend(HaruWindowBackend BACKEND);
+
+    HaruWindow *HaruCreateWindow(const char *Title, int WIDTH, int HEIGHT);
+    void HaruDestroyWindow(HaruWindow *WINDOW);
+    HaruResult HaruInitializeWindowing();
+
+    void HaruWindowPollEvents(HaruWindow *WINDOW);
+    int HaruWindowShouldClose(HaruWindow *WINDOW);
+
+#endif
