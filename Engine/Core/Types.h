1e90f56871c44338729c789a51e0734d2d65fdc6
Averi <averyb.personal@gmail.com>
2026-04-06 23:49:51 -0400

    Create Types.h

---
+++ Engine/Core/Types.h
@@ -0,0 +1,43 @@
+//
+//  Haruhi Engine
+//
+//  Original source Code derived from Kunamo Engine:
+//    Copyright © 2026 Kunamo Entertainment. All rights reserved.
+//
+//  Modifications & Integration:
+//    Copyright © 2026 AveriC & Averi
+//
+
+#ifndef CORE_TYPES_H
+#define CORE_TYPES_H
+
+    #include <stdint.h>
+    #include <stddef.h>
+    #include <setjmp.h>
+
+    typedef enum {
+        HARU_FALSE = 0,
+        HARU_TRUE = 1
+    } HaruBoolean;
+
+    typedef enum {
+        HARU_EXIT_FAILURE = -1,
+        HARU_EXIT_SUCCESS = 0
+    } HaruExitCode;
+
+    typedef enum {
+        HARU_RESULT_SUCCESS = 1,
+        HARU_RESULT_FAILURE = 0,
+        HARU_RESULT_ERROR = -1,
+    } HaruResult;
+
+    typedef struct {
+        jmp_buf Environment;
+        int Code;
+    } HaruException;
+
+    #define TRY(Context) if (((Context).Code = setjmp((Context).Environment)) == 0)
+    #define CATCH(Context) else
+    #define THROW(Context, Error) do { (Context).Code = (Error); longjmp((Context).Environment, (Error)); } while(0)
+
+#endif
