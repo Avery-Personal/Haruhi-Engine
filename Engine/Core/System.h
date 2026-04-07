2b91cab6b72b68b35d2c1051ad5355780d8869d1
Averi <averyb.personal@gmail.com>
2026-04-06 23:49:54 -0400

    Create System.h

---
+++ Engine/Core/System.h
@@ -0,0 +1,17 @@
+//
+//  Haruhi Engine
+//
+//  Original source code derived from Kunamo Engine:
+//    Copyright © 2026 Kunamo Entertainment. All rights reserved.
+//
+//  Modifications & Integration:
+//    Copyright © 2026 AveriC & Averi
+//
+
+#ifndef CORE_SYSTEMS_H
+#define CORE_SYSTEMS_H
+
+    #include "Types.h"
+    #include "Versioning.h"
+
+#endif
