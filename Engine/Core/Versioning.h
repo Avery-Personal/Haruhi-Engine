b2603dde40d32c7eaf585419f0a3389a3aa25cba
Averi <averyb.personal@gmail.com>
2026-04-06 23:49:49 -0400

    Create Versioning.h

---
+++ Engine/Core/Versioning.h
@@ -0,0 +1,26 @@
+//
+//  Haruhi Engine
+//
+//  Original source code derived from Kunamo Engine:
+//    Copyright © 2026 Kunamo Entertainment. All rights reserved.
+//
+//  Modifications & Integration:
+//    Copyright © 2026 AveriC & Averi
+//
+
+#ifndef CORE_VERSIONING_H
+#define CORE_VERSIONING_H
+
+    #include <stdint.h>
+
+    typedef uint32_t HaruVersion;
+
+    #define HARU_MAKE_VERSION(Major, Minor, Patch) (((uint32_t)(Major) << 22) | ((uint32_t)(Minor) << 12) | ((uint32_t)(Patch)))
+
+    #define HARU_VERSION_MAJOR(Version) ((uint32_t)(Version) >> 22)
+    #define HARU_VERSION_MINOR(Version) (((uint32_t)(Version) >> 12) & 0x3FF)
+    #define HARU_VERSION_PATCH(Version) ((uint32_t)(Version) & 0xFFF)
+
+    #define HARU_ENGINE_VERSION HARU_MAKE_VERSION(0, 1, 0)
+
+#endif
