196510a5e91d6cee9145714b7b38e8d096a63d82
Averi <averyb.personal@gmail.com>
2026-04-06 23:50:11 -0400

    Update HaruBoot.h

--- Engine/Context/Boot/HaruBoot.h
+++ Engine/Context/Boot/HaruBoot.h
@@ -6,9 +6,4 @@
     #define HARUHI_STANDARDIZED_ENTRY_POINT int
     #define HARUHI_CONTEXTUAL_ENTRY_POINT HaruContext
 
-    typedef enum {
-        HARU_EXIT_SUCCESS = 0,
-        HARU_EXIT_FAILURE = -1
-    } HaruhiExitCode;
-
 #endif
