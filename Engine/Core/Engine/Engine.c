e3ee13042f1fa1e9572c3e9b06d998c50592f2ed
Averi <averyb.personal@gmail.com>
2026-04-13 18:00:02 -0400

    Update Engine.c

--- Engine/Core/Engine/Engine.c
+++ Engine/Core/Engine/Engine.c
@@ -1,3 +1,18 @@
+//
+//  Haruhi Engine
+// 
+//  This file is of derivation from Kunamo Engine source code & has been adapted for integration of Haruhi Engine.
+//  The following software is DIRECTLY seeded from Kunamo Engine source with minuscle adjustments to architecture, if any.
+//
+//  Portions or sectors of this file originate from Kunamo Engine source code and may include minimal modifications
+// 
+//  Original source code derived from Kunamo Engine:
+//    Copyright © 2026 Kunamo Entertainment. All rights reserved.
+//
+//  Modifications & Integration:
+//    Copyright © 2026 AveriC & Averi
+//
+
 #include "Engine.h"
 
 HaruEngine HaruEngineInitialize() {
