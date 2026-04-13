564e57847d97aba6348ee1a2f4d8c5244cbd99a4
Averi <averyb.personal@gmail.com>
2026-04-13 16:36:12 -0400

    Create Timer.h

---
+++ Engine/Platform/Runtime/Timer/Timer.h
@@ -0,0 +1,36 @@
+#ifndef HARU_TIMER_H
+#define HARU_TIMER_H
+
+    #include <stdint.h>
+
+    typedef double HaruTime;
+
+    typedef struct {
+        HaruTime Start;
+        HaruTime Elapsed;
+
+        int Running;
+    } HaruTimer;
+
+    typedef struct {
+        HaruTime DeltaTime;
+        HaruTime TotalTime;
+        HaruTime LastFrame;
+    } HaruTimeContext;
+
+    void HaruTimeInitialize(HaruTimeContext *Context);
+    void HaruTimeUpdate(HaruTimeContext *Context);
+
+    void HaruTimerStart(HaruTimer *Timer);
+    void HaruTimerStop(HaruTimer *Timer);
+    void HaruTimerReset(HaruTimer *Timer);
+    HaruTime HaruTimerElapsed(HaruTimer *Timer);
+
+    HaruTime HaruTimeNow(void);
+
+    const char *HaruTimeFormat(HaruTime Time);
+
+    #define HARU_TIMER_BEGIN(Timer) HaruTimerStart(Timer)
+    #define HARU_TIMER_END(Timer) HaruTimerStop(Timer)
+
+#endif
