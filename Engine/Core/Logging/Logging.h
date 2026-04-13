5310c664de1785ae4b835f438ec375aac1fc06ce
Averi <averyb.personal@gmail.com>
2026-04-12 21:35:21 -0400

    Update Logging.h

--- Engine/Core/Logging/Logging.h
+++ Engine/Core/Logging/Logging.h
@@ -1,6 +1,7 @@
 #ifndef HARUHI_LOGGING_H
 #define HARUHI_LOGGING_H
 
+    #include <stdio.h>
     #include <stdint.h>
 
     typedef enum {
@@ -11,5 +12,14 @@
         HARU_LOGGING_ERROR,
         HARU_LOGGING_FATAL
     } HaruLoggingLevel;
+
+    typedef struct HaruLogger {
+        FILE *File;
+
+        uint8_t EnableColors;
+        uint8_t EnableFile;
+        
+        HaruLoggingLevel Level;
+    } HaruLogger;
 
 #endif
