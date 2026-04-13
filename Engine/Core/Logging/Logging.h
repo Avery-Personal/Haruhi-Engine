e658a63835825a7f9b4d60cb302d1faeb879112d
Averi <averyb.personal@gmail.com>
2026-04-12 21:41:11 -0400

    Update Logging.h

--- Engine/Core/Logging/Logging.h
+++ Engine/Core/Logging/Logging.h
@@ -4,6 +4,14 @@
     #include <stdio.h>
     #include <stdint.h>
 
+    #define HARU_COLOR_RESET "\033[0m"
+    #define HARU_COLOR_TRACE "\033[37m"
+    #define HARU_COLOR_DEBUG "\033[36m"
+    #define HARU_COLOR_INFO "\033[32m"
+    #define HARU_COLOR_WARN "\033[33m"
+    #define HARU_COLOR_ERROR "\033[31m"
+    #define HARU_COLOR_FATAL "\033[41m\033[37m"
+
     typedef enum {
         HARU_LOGGING_TRACE,
         HARU_LOGGING_DEBUG,
@@ -23,7 +31,7 @@
     } HaruLogger;
 
     void HaruLoggerInitialize(HaruLogger *Logger);
-    
+
     void HaruLoggerSetFile(HaruLogger *Logger, const char *Path);
     void HaruLoggerSetLevel(HaruLogger *Logger, HaruLoggingLevel Level);
 
