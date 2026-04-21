379b858f080b600da56ebdf089409b5a1d90e908
Averi <averyb.personal@gmail.com>
2026-04-21 12:14:36 -0400

    Update HaruEngineForth.c

--- Engine/Core/Debug/Forth/HaruEngineForth.c
+++ Engine/Core/Debug/Forth/HaruEngineForth.c
@@ -3,32 +3,77 @@
 #include "../../Logging/Logging.h"
 
 static int WordLogTrace(HaruForthContext *Context) {
-    HARU_LOG_TRACE(&gLogger, "Forth trace command");
+    HaruForthValue Value;
+
+    if (!HaruForthPop(Context, &Value))
+        return 0;
+
+    if (Value.Type == HARU_FORTH_TYPE_NUMBER) {
+        HARU_LOG_TRACE(&gLogger, "Forth: %g", Value.As.Number);
+    } else if (Value.Type == HARU_FORTH_TYPE_STRING) {
+        HARU_LOG_TRACE(&gLogger, "%s", Value.As.String);
+    }
     
     return 1;
 }
 
 static int WordLogDebug(HaruForthContext *Context) {
-    HARU_LOG_DEBUG(&gLogger, "Forth debug command");
+    HaruForthValue Value;
+
+    if (!HaruForthPop(Context, &Value))
+        return 0;
+
+    if (Value.Type == HARU_FORTH_TYPE_NUMBER) {
+        HARU_LOG_DEBUG(&gLogger, "Forth: %g", Value.As.Number);
+    } else if (Value.Type == HARU_FORTH_TYPE_STRING) {
+        HARU_LOG_DEBUG(&gLogger, "%s", Value.As.String);
+    }
     
     return 1;
 }
 
 static int WordLogInfo(HaruForthContext *Context) {
-    HARU_LOG_INFO(&gLogger, "Forth info command");
+    HaruForthValue Value;
+
+    if (!HaruForthPop(Context, &Value))
+        return 0;
+
+    if (Value.Type == HARU_FORTH_TYPE_NUMBER) {
+        HARU_LOG_INFO(&gLogger, "Forth: %g", Value.As.Number);
+    } else if (Value.Type == HARU_FORTH_TYPE_STRING) {
+        HARU_LOG_INFO(&gLogger, "%s", Value.As.String);
+    }
     
     return 1;
 }
 
 static int WordLogWarn(HaruForthContext *Context) {
-    HARU_LOG_WARN(&gLogger, "Forth warn command");
+    HaruForthValue Value;
+
+    if (!HaruForthPop(Context, &Value))
+        return 0;
+
+    if (Value.Type == HARU_FORTH_TYPE_NUMBER) {
+        HARU_LOG_WARN(&gLogger, "Forth: %g", Value.As.Number);
+    } else if (Value.Type == HARU_FORTH_TYPE_STRING) {
+        HARU_LOG_WARN(&gLogger, "%s", Value.As.String);
+    }
     
     return 1;
 }
 
 static int WordLogError(HaruForthContext *Context) {
-    HARU_LOG_ERROR(&gLogger, "Forth error command");
+    HaruForthValue Value;
 
+    if (!HaruForthPop(Context, &Value))
+        return 0;
+
+    if (Value.Type == HARU_FORTH_TYPE_NUMBER) {
+        HARU_LOG_ERROR(&gLogger, "Forth: %g", Value.As.Number);
+    } else if (Value.Type == HARU_FORTH_TYPE_STRING) {
+        HARU_LOG_ERROR(&gLogger, "%s", Value.As.String);
+    }
+    
     return 1;
 }
 
@@ -37,7 +82,7 @@ static int WordTimeDelta(HaruForthContext *Context) {
     if (!Application)
         return 0;
 
-    return HaruForthPush(Context, Application -> Time.DeltaTime);
+    return HaruForthPushNumber(Context, Application -> Time.DeltaTime);
 }
 
 static int WordTimeTotal(HaruForthContext *Context) {
@@ -45,7 +90,7 @@ static int WordTimeTotal(HaruForthContext *Context) {
     if (!Application)
         return 0;
 
-    return HaruForthPush(Context, Application -> Time.TotalTime);
+    return HaruForthPushNumber(Context, Application -> Time.TotalTime);
 }
 
 static int WordWindowCount(HaruForthContext *Context) {
@@ -53,7 +98,7 @@ static int WordWindowCount(HaruForthContext *Context) {
     if (!Application)
         return 0;
 
-    return HaruForthPush(Context, (double) Application -> WindowCount);
+    return HaruForthPushNumber(Context, (double) Application -> WindowCount);
 }
 
 static int WordQuit(HaruForthContext *Context) {
