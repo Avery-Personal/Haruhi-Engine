8959959e515d4d0b755ac7e9454635aef39a940e
Averi <averyb.personal@gmail.com>
2026-04-21 10:26:04 -0400

    Update Types.h

--- Engine/Core/Types.h
+++ Engine/Core/Types.h
@@ -11,9 +11,17 @@
 #ifndef CORE_TYPES_H
 #define CORE_TYPES_H
 
-    #include <stdint.h>
-    #include <stddef.h>
     #include <setjmp.h>
+
+    typedef signed char i8;
+    typedef signed short i16;
+    typedef signed int i32;
+    typedef signed long long i64;
+
+    typedef unsigned char u8;
+    typedef unsigned short u16;
+    typedef unsigned int u32;
+    typedef unsigned long long u64;
 
     typedef enum {
         HARU_FALSE = 0,
