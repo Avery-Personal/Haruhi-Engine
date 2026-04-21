d5d3f7fe41616e3a4bbd552589591eb489fde873
Averi <averyb.personal@gmail.com>
2026-04-21 10:26:17 -0400

    Create HaruEngineForth.h

---
+++ Engine/Core/Debug/Forth/HaruEngineForth.h
@@ -0,0 +1,10 @@
+#ifndef HARU_ENGINE_FORTH_H
+#define HARU_ENGINE_FORTH_H
+
+    #include "Forth.h"
+
+    struct HaruApplication;
+
+    void HaruForthRegisterEngineWords(HaruForthContext *Context);
+
+#endif
