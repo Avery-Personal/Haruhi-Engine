9718f4fd070b2bca4115190df6a6bf4d3224cb52
Averi <averyb.personal@gmail.com>
2026-04-24 21:36:45 -0400

    Create ECS.h

---
+++ Engine/Runtime/ECS/ECS.h
@@ -0,0 +1,44 @@
+#ifndef ECS_H
+#define ECS_H
+
+    #include "../../Core/System.h"
+
+    typedef u64 EntityID;
+
+    typedef enum {
+        COMPONENT_TRANSFORM,
+        COMPONENT_RENDER,
+        COMPONENT_PHYSICS,
+        COMPONENT_AUDIO,
+        COMPONENT_SCRIPT
+    } ComponentType;
+
+    typedef struct Instance Instance;
+    typedef struct Component Component;
+
+    typedef struct Instance {
+        const char *Name;
+
+        EntityID ID;
+
+        Instance *Parent;
+        Instance **Children;
+
+        u32 ChildCount;
+        u32 ChildCapacity;
+
+        Component **Components;
+
+        u32 ComponentCount;
+        u32 ComponentCapacity;
+
+        u32 Flags;
+    } Instance;
+
+    typedef struct Component {
+        ComponentType Type;
+
+        void *Data;
+    } Component;
+
+#endif
