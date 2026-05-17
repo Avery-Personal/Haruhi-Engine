aedab53b201ef9c0f9f7f7f3e79c6632c71b8516
Averi <averyb.personal@gmail.com>
2026-05-17 09:34:27 -0400

    Create Renderer.h

---
+++ Engine/Systems/Rendering/Renderer.h
@@ -0,0 +1,86 @@
+//
+//  Haruhi Engine
+//
+//  Copyright © 2026 AveriC & Averi
+//
+
+#ifndef RENDERER_H
+#define RENDERER_H
+
+    #include "../Core/System.h"
+
+    typedef struct HaruRenderer HaruRenderer;
+
+    typedef struct {
+        float R, G, B, A;
+    } HaruColor;
+
+    typedef struct {
+        float X, Y;
+    } HaruVec2;
+
+    typedef struct {
+        HaruVec2 Position;
+        HaruColor Color;
+    } HaruVertex;
+
+    typedef enum {
+        HARU_RENDERER_TOPOLOGY_TRIANGLES,
+        HARU_RENDERER_TOPOLOGY_LINES
+    } HaruTopology;
+
+    typedef struct {
+        HaruTopology Topology;
+        HaruBoolean EnableDepth;
+    } HaruPipelineDesc;
+
+    typedef struct {
+        int Handle;
+    } HaruMesh;
+
+    typedef struct {
+        int Handle;
+    } HaruPipeline;
+
+    typedef struct {
+        void *VertexBuffer; // TO FIND Rendering API
+        
+        int VertexCount;
+    } HaruMeshInternal;
+
+    typedef struct {
+        void *Pipeline; // TO FIND Rendering API
+    } HaruPipelineInternal;
+
+    typedef struct HaruRenderer {
+        int Width;
+        int Height;
+
+        HaruBoolean FrameActive;
+
+        void *PassAction; // TO FIND Rendering API
+
+        void *DefaultPipeline; // TO FIND Rendering API
+        void *Bindings; // TO FIND Rendering API
+
+        HaruMeshInternal Meshes[256];
+        int MeshCount;
+
+        HaruPipelineInternal Pipelines[64];
+        int PipelineCount;
+    } HaruRenderer;
+
+    HaruRenderer *HaruRendererCreate(int Width, int Height);
+    void HaruRendererDestroy(HaruRenderer *Renderer);
+
+    void HaruRendererBeginFrame(HaruRenderer *Renderer, HaruColor ClearColor);
+    void HaruRendererEndFrame(HaruRenderer *Renderer);
+
+    HaruMesh HaruRendererCreateMesh(HaruRenderer *Renderer, const HaruVertex *Vertices, int VertexCount);
+    void HaruRendererDestroyMesh(HaruRenderer *Renderer, HaruMesh Mesh);
+
+    HaruPipeline HaruRendererCreatePipeline(HaruRenderer *Renderer, HaruPipelineDesc Desc);
+
+    void HaruRendererDrawMesh(HaruRenderer *Renderer, HaruMesh Mesh, HaruPipeline Pipeline);
+
+#endif
