094fd014565a999f7c6cc144e71b27d51cb67b37
Averi <averyb.personal@gmail.com>
2026-07-27 00:19:10 -0400

    Update Scene.h

--- Engine/Systems/Rendering/Scene.h
+++ Engine/Systems/Rendering/Scene.h
@@ -9,10 +9,25 @@
 
     #include "Renderer.h"
 
+    typedef struct {
+        HaruMesh Mesh;
+        HaruPipeline Pipeline;
+
+        float PositionX, PositionY, PositionZ;
+        float Scale;
+
+        HaruColor Color;
+    } HaruEntity;
+
+    typedef struct {
+        HaruEntity Entities[MAX_ENTITIES];
+        int EntityCount;
+    } HaruScene;
+
     HaruScene HaruSceneCreate(void);
 
     HaruBoolean HaruSceneAddEntity(HaruScene *Scene, HaruEntity Entity);
-    HaruBoolean HaruSceneSpawnEntity(HaruScene *Scene, HaruMesh Mesh, HaruPipeline Pipeline, float X, float Y, float Z, float Scale);
+    HaruBoolean HaruSceneSpawnEntity(HaruScene *Scene, HaruMesh Mesh, HaruPipeline Pipeline, float X, float Y, float Z, float Scale, HaruColor Color);
     
     void HaruRendererDrawScene(HaruRenderer *Renderer, const HaruScene *Scene);
     void HaruSceneClear(HaruScene *Scene);
